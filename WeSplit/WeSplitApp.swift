dffd77f181cf12a3edf707d690539038fba5c202
leotis <mekdemcrossit@tutanota.com>
2021-04-21 05:35:31 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Leotis buchanan on 2021-04-21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
